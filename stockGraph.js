13f6ba9d9f605130fae0cd8ac31500e3b08e98c0
m-aguirre <mpaguirre@dons.usfca.edu>
2017-10-23 14:49:34 -0700

    Adds y-coordinate scale to left side of view pane

--- stockGraph.js
+++ stockGraph.js
@@ -6,34 +6,59 @@ var tempData = [
   {date: 2012-10-12, close: 87.69}
 ]
 
-
-
-var axes = {
-  x: d3.scaleLinear().domain([0, 100]).range([0, 200]),
-  y: d3.scaleLinear().domain([0, 100]).range([0, 400])
-};
+// var axes = {
+//   x: d3.scaleLinear().domain([0, 100]).range([0, 600]),
+//   y: d3.scaleLinear().domain([0, 100]).range([0, 400])
+// };
 
 var x = d3.scaleTime()
     .domain([
       new Date(Date.parse('2014-01-01')),
-      new Date(Date.parse('2014-04-01'))
+      new Date(Date.parse('2015-01-01'))
     ])
-    .range([0, 300]);
+    .range([0, 600]);
 
-var xAxis = d3.axisBottom(x).ticks(4);
+//idea for y-scale: use d3.range or d3.ticks to create array of y-scale arguments
+//want an array of linearly-spaced values from min - 10ish% to max + 10ish%
+//var y = d3.ticks(0,100, 25);
+var yScale = d3.scaleLinear()
+    .domain([100,0])
+    .range([35,400]);
+
+    var y = d3.scaleTime()
+        .domain([
+          new Date(Date.parse('2014-01-01')),
+          new Date(Date.parse('2015-01-01'))
+        ])
+        .range([0, 600]);
+
+var xAxis = d3.axisBottom(x).ticks(14);
+var yAxis = d3.axisLeft(yScale).ticks(6);
 
 const graph = () => {
   d3.select('body')
     .append('svg') //later, add div to html and select it
     .attr('class', 'viewport')
-    .attr('width', 300)
-    .attr('height', 250)
+    .attr('width', 700)
+    .attr('height', 450)
     .style('background-color', 'grey')
-    .append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + (230) + ')') //putting it at 'height' (== 250) pushes scale off the graph
-    .call(xAxis)
+
 }
 
+const placeXAxis = () => {
+  d3.select('.viewport')
+  .append('g')
+  .attr('transform', 'translate(50,' + (400) + ')') //putting it at 'height' (== 250) pushes scale off the graph
+  .call(xAxis)
+}
+
+const placeYAxis = () => {
+  d3.select('.viewport')
+  .append('g')
+  .attr('transform', 'translate(50,' + 0 + ')')
+  .call(yAxis)
+}
 
 graph();
+placeXAxis();
+placeYAxis();
